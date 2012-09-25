bd849512082da43abf3e454fe322ae9151c1c587
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-09-25 14:23:25 -0400

    Empty project

---
+++ View Switcher/View Switcher/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  View Switcher
+//
+//  Created by Laïla Atrmouh on 25/09/12.
+//  Copyright (c) 2012 URI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ View Switcher/View Switcher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  View Switcher
+//
+//  Created by Laïla Atrmouh on 25/09/12.
+//  Copyright (c) 2012 URI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
